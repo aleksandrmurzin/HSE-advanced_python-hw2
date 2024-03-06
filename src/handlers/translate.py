8df263313a22794cb91a04e3603fcdf57a336835
Aleksandr Murzin <aleksandr.murzin@yahoo.com>
2024-03-06 11:03:36 +0300

    fix handling long text
    fix handling no loaded model

--- src/handlers/translate.py
+++ src/handlers/translate.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 from aiogram import Router, types
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
@@ -13,23 +15,41 @@ class Translate(StatesGroup):
     text = State()
 
 
+@dataclass
 class Content:
-    init = as_list(
+    status: bool = True
+
+    @property
+    def working_status(self):
+        return as_list(
         as_marked_section(
             Bold("Статус:"),
             "Перевожу...",
             marker="⏳ ",
         ),
     )
-
-    update = as_list(
-        as_marked_section(
+    
+    def status_section(self):
+        if not self.status:
+            return as_marked_section(
+                Bold("Статус:"),
+                "Перевод не выполнен!",
+                marker="⚠️ ",
+            )
+        return as_marked_section(
             Bold("Статус:"),
             "Успех!",
             marker="✅ ",
-        ),
+        )
+    
+
+ 
+    @property
+    def updated_status(self):
+        return as_list(
+        self.status_section(),
         as_marked_section(
-            Bold("Ваш перевод сообщением ниже!"),
+            "Ответ",
             "",
             marker="⬇",
         ),
@@ -48,12 +68,8 @@ async def cmd_translate(message: types.Message, state: FSMContext) -> None:
 async def process_text(message: types.Message, state: FSMContext) -> None:
     await state.clear()
 
-    init_message = await message.reply(**Content.init.as_kwargs())
-    translation = translator.translate(message.text)
+    init_message = await message.reply(**Content().working_status.as_kwargs())
+    translation, status = translator.translate(message.text)
 
-    if translation:
-        await init_message.edit_text(**Content.update.as_kwargs())
-        await message.answer(f"{translation}")
-
-    else:
-        ...  # add reject
+    await init_message.edit_text(**Content(status=status).updated_status.as_kwargs())
+    await message.answer(f"{translation}")
