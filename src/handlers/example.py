699d75e96a22b2810f882756b09a838cc387a39b
Aleksandr Murzin <aleksandr.murzin@yahoo.com>
2024-03-18 09:52:10 +0300

    del unnessesary files

--- src/handlers/example.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Router, types
-from aiogram.filters import Command
-
-router = Router()
-
-
-@router.message(Command("test"))
-async def cmd_start(message: types.Message):
-    await message.answer(str(message.chat.type))
