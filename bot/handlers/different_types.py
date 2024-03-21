44ec25a7d33009e214ef8f86c714181d91a30e9a
Aleksandr Murzin <aleksandr.murzin@yahoo.com>
2024-03-21 08:42:19 +0300

    fix handling different types

--- bot/handlers/different_types.py
+++ bot/handlers/different_types.py
@@ -6,24 +6,10 @@ from bot.filters.chat_type import ChatTypeFilter
 router = Router()
 router.message.filter(ChatTypeFilter())
 
+@router.message(~F.text)
+async def process_non_text(message: Message) -> None:
+    await message.answer("К сожалению, я полезен лишь при вызове команд, попробуте нажать /translate, чтобы сделать перевод.")
 
-@router.message(F.sticker)
-async def message_with_sticker(message: Message):
-    """
-    :param message: message
-    """
-    await message.answer("Это стикер, но я не умею их переводить")
-
-
-@router.message(F.animation)
-async def message_with_gif(message: Message):
-    """
-    This function is triggered when a user sends an animation as a message.
-
-    :param message: The incoming message object
-    :type message: aiogram.types.Message
-    :return: None
-    """
-    await message.answer(
-        "This message contains an animation, but I don't know how to translate it"
-    )
+@router.message(F.text)
+async def process_text(message: Message) -> None:
+    await message.answer("К сожалению, я полезен лишь при вызове команд, попробуте нажать /translate, чтобы сделать перевод.")
